b1a6aea862023cdba1b5361840f6c6303240afc9
NadetDev <nadetdev@gmail.com>
2024-04-01 00:23:32 +0200

    Add view + model data details

--- app/routes/client/view.js
+++ app/routes/client/view.js
@@ -1,3 +1,27 @@
 import Route from '@ember/routing/route';
 
-export default class ClientViewRoute extends Route {}
+export default class ClientViewRoute extends Route {
+  model(params) {
+    let clientId = params.client_id;
+
+    if (clientId === '1') {
+      return {
+        id: 1,
+        name: 'NadetDev',
+        company_name: 'Web Dev Co',
+        email: 'info@nadet-dev.com',
+        phone_number: '+33 01 0 03 04 05',
+        active: true,
+      };
+    } else if (clientId === '2') {
+      return {
+        id: 2,
+        name: 'NadetSoft',
+        company_name: 'Software Co',
+        email: 'contact@nadetsoft.com',
+        phone_number: '+33 03 04 05 06 07',
+        active: true,
+      };
+    }
+  }
+}
