5fdbecb7a4792385190f38ebaa7de613e6f28f60
NadetDev <nadetdev@gmail.com>
2024-04-03 20:15:26 +0200

    route, action, error, transition

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ export default class Router extends EmberRouter {
 Router.map(function () {
   this.route('about');
   this.route('client', function () {
-    this.route('view', {path: '/:client_id'});
+    this.route('view', { path: '/:client_id' });
   });
+  this.route('fourohfour', { path: '/*' });
 });
--- app/routes/client/view.js
+++ app/routes/client/view.js
@@ -1,8 +1,29 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import { later } from '@ember/runloop';
+import { action } from '@ember/object';
 
 export default class ClientViewRoute extends Route {
   @service router;
+  @action
+  loading(transition) {
+    // eslint-disable-next-line ember/no-controller-access-in-routes
+    let controller = this.controllerFor('client.view');
+    controller.set('isLoading', true);
+    transition.promise.finally(() => {
+      controller.set('isLoading', false);
+    });
+    return true;
+  }
+
+  @action
+  error(error, transition) {
+    if (error.status == 404) {
+      console.log('404 error');
+      this.router.transitionTo('/fourohfour');
+    }
+    return true;
+  }
 
   beforeModel(transition) {
     let clientId = transition.to.params.client_id;
@@ -12,27 +33,46 @@ export default class ClientViewRoute extends Route {
     }
   }
 
-  model(params) {
-    let clientId = params.client_id;
-
-    if (clientId === '1') {
-      return {
-        id: 1,
-        name: 'NadetDev',
-        company_name: 'Web Dev Co',
-        email: 'info@nadet-dev.com',
-        phone_number: '+33 01 0 03 04 05',
-        active: true,
-      };
-    } else if (clientId === '2') {
-      return {
-        id: 2,
-        name: 'NadetSoft',
-        company_name: 'Software Co',
-        email: 'contact@nadetsoft.com',
-        phone_number: '+33 03 04 05 06 07',
-        active: true,
-      };
+  afterModel(model) {
+    if (!model.active) {
+      this.router.transitionTo('client');
     }
+  }
+
+  setupController(controller, model) {
+    controller.set('testVar', 'Testing Var');
+    // eslint-disable-next-line ember/no-controller-access-in-routes
+    this.controllerFor('client').set('clientModuleTitle', model.name);
+  }
+
+  model(params) {
+    return new Promise((resolve, reject) => {
+      later(() => {
+        console.log('Completing promise');
+        let clientId = params.client_id;
+
+        if (clientId === '1') {
+          resolve({
+            id: 1,
+            name: 'NadetDev',
+            company_name: 'Web Dev Co',
+            email: 'info@nadet-dev.com',
+            phone_number: '+33 01 0 03 04 05',
+            active: true,
+          });
+        } else if (clientId === '2') {
+          resolve({
+            id: 2,
+            name: 'NadetSoft',
+            company_name: 'Software Co',
+            email: 'contact@nadetsoft.com',
+            phone_number: '+33 03 04 05 06 07',
+            active: true,
+          });
+        } else {
+          reject({ status: 404, detail: 'Client not found' });
+        }
+      }, 1500);
+    });
   }
 }
