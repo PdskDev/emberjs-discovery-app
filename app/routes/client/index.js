25641c9a99ce9fde4f12a3b026fa477d4189d889
NadetDev <nadetdev@gmail.com>
2024-04-03 21:18:09 +0200

    Router handler - end

--- app/routes/client/index.js
+++ app/routes/client/index.js
@@ -1,3 +1,26 @@
 import Route from '@ember/routing/route';
 
-export default class ClientIndexRoute extends Route {}
+export default class ClientIndexRoute extends Route {
+  queryParams = ['sortBy', 'order'];
+
+  sortBy = null;
+  order = null;
+
+  setupController() {
+    // eslint-disable-next-line ember/no-controller-access-in-routes
+    this.controllerFor('client').set('clientModuleTitle', 'Clients');
+  }
+
+  /* queryParams = {
+    sortBy: {
+      refreshModel: false,
+    },
+    order: {
+      refreshModel: true,
+    },
+  }; */
+
+  model(params) {
+    console.log(params);
+  }
+}
